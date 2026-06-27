d22c3220e1b8b9638a57446895be252db17f5a58
rashifanurrahim <rashifa.nurrahim@gmail.com>
2026-06-27 11:52:28 +0700

    UI responsive fixes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -53,7 +53,11 @@ function Navbar() {
       <div className="navbar-start">
         {/* MOBILE */}
         <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+          <div
+            tabIndex={0}
+            role="button"
+            className="btn btn-ghost lg:hidden text-zinc-800"
+          >
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="h-5 w-5"
